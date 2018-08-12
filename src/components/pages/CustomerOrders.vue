02905c340363133327204ff43da1676b65b7761d
Yeh Jing <yeh845251@gmail.com>
2018-08-12 17:01:43 +0800

    模擬訂單 選取單一產品 Loading動畫 Modal

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -6,7 +6,7 @@
     <div class="row mt-4">
       <div class="col-md-4 mb-4" v-for="item in products" :key="item.id">
         <div class="card border-0 shadow-sm">
-          <div style="height: 150px; background-size: cover; background-position: center"
+          <div style="height: 300px; background-size: cover; background-position: center"
             :style="{backgroundImage : `url(${item.imageUrl})`}">
           </div>
           <div class="card-body">
@@ -14,7 +14,7 @@
             <h5 class="card-title">
               <a href="#" class="text-dark">{{ item.title }}</a>
             </h5>
-            <p class="card-text">{{ item.content }}</p>
+            <p class="card-text">{{ item.description }}</p>
             <div class="d-flex justify-content-between align-items-baseline">
               <div class="h5" v-if="!item.price">{{ item.origin_price }}</div>
               <del class="h6" v-if="item.price">原價 {{ item.origin_price }} 元</del>
@@ -22,12 +22,53 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fas fa-spinner fa-spin"></i>
+            <button type="button" class="btn btn-outline-secondary btn-sm" @click="getProduct(item.id)">
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fas fa-spinner fa-spin"></i>
+              <i class="fas fa-spinner fa-spin" v-if="status.loadingItem === item.id"></i>
+              加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade" id="productModal" tabindex="-1" role="dialog"
+      aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">{{ product.title }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.imageUrl" class="img-fluid" alt="">
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.description }}</p>
+              <footer class="blockquote-footer mt-3">{{ product.content }}</footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">{{ product.origin_price }} 元</div>
+              <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
+              <div class="h4" v-if="product.price">現在只要 {{ product.price }} 元</div>
+            </div>
+            <select name="" class="form-control mt-3" v-model="product.num">
+              <option :value="num" v-for="num in 10" :key="num">
+                選購 {{num}} {{product.unit}}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong>{{ product.num * product.price }}</strong> 元
+            </div>
+            <button type="button" class="btn btn-primary"
+              @click="addtoCart(product.id, product.num)">
+              <!-- <i class="fas fa-spinner fa-spin" v-if="product.id === status.loadingItem"></i> -->
               加到購物車
             </button>
           </div>
@@ -38,16 +79,22 @@
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
   data() {
     return{
       products: [],
+      product: {},
       isLoading: false,
+      status:{
+        loadingItem:''
+      }
     };
   },
   methods:{
     getProducts() {
-      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products`;
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products/all`;
       const vm = this;
       vm.isLoading = true;
       this.$http.get(api).then(response => {
@@ -55,6 +102,19 @@ export default {
         vm.isLoading = false;
       });
     },
+    // 取得單一商品細節 開啟 Modal
+    getProduct(id) {
+      const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      this.$http.get(api).then(response => {
+        // 先取得資料再開啟 Modal
+        vm.product = response.data.product;
+        console.log(response.data)
+        $('#productModal').modal('show');
+        vm.status.loadingItem = '';
+      });
+    }
   },
   created() {
     this.getProducts();
@@ -62,3 +122,8 @@ export default {
 }
 
 </script>
+<style scoped>
+.blockquote-footer:before{
+  content:''
+}
+</style>
