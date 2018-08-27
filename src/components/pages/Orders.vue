588520c9f1e7d07dee2ec309b9bf5ae54c2f4f56
Yeh Jing <yeh845251@gmail.com>
2018-08-27 22:32:43 +0800

    完成後台 => 訂單列表

--- src/components/pages/Orders.vue
+++ src/components/pages/Orders.vue
@@ -1,3 +1,112 @@
 <template>
-  
+  <div>
+    <Loading :active.sync="isLoading"></Loading>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th>購買時間</th>
+          <th>Email</th>
+          <th>購買款項</th>
+          <th class="text-right">應付金額</th>
+          <th>是否付款</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, key) in sortOrder" :key="key"
+          v-if="orders.length"
+          :class="{'text-secondary': !item.is_paid}">
+          <td>{{ item.create_at | date }}</td>
+          <td><span v-text="item.user.email" v-if="item.user"></span></td>
+          <td>
+            <ul class="list-unstyled">
+              <li v-for="(product, i) in item.products" :key="i">
+                {{ product.product.title }} 數量：{{ product.qty }}
+                {{ product.product.unit }}
+              </li>
+            </ul>
+          </td>
+          <td class="text-right">{{ item.total | currency }}</td>
+          <td>
+            <strong v-if="item.is_paid" class="text-success">已付款</strong>
+            <span v-else class="text-danger">尚未付款</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- 分頁 start -->
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item" :class="{ 'disabled' : !pagination.has_pre }">
+          <a class="page-link" href="#" aria-label="Previous" 
+            @click.prevent="getProducts(pagination.current_page - 1)">
+            <span aria-hidden="true">&laquo;</span>
+            <span class="sr-only">Previous</span>
+          </a>
+        </li>
+        <li class="page-item" v-for="page in pagination.total_pages" :key="page"
+          :class="{'active' : pagination.current_page === page}">
+          <a class="page-link" href="#" @click.prevent="getProducts(page)">{{page}}</a>
+        </li>
+        <li class="page-item" :class="{'disabled' : !pagination.has_next}">
+          <a class="page-link" href="#" aria-label="Next" 
+            @click.prevent="getProducts(pagination.current_page + 1)">
+            <span aria-hidden="true">&raquo;</span>
+            <span class="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- 分頁 end -->
+
+    <!-- <Pagination :pages="pagination" @emitPages="getOrders"></Pagination> -->
+  </div>
 </template>
+<script>
+// import Pagination from '../Pagination';
+export default {
+  data() {
+    return {
+      orders: {},
+      isNew: false,
+      pagination: {},
+      isLoading: false,
+      Pagination: {}
+    };
+  },
+  // components: {
+  //   Pagination,
+  // },
+  methods: {
+    getOrders(currentPage = 1) {
+      const vm = this;
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/admin/orders?page=${currentPage}`;
+      vm.isLoading = true;
+      this.$http.get(url, vm.tempProduct).then((response) => {
+        vm.orders = response.data.orders;
+        vm.pagination = response.data.pagination;
+        vm.isLoading = false;
+        console.log(response);
+      });
+    },
+  },
+  computed: {
+    sortOrder() {
+      const vm = this;
+      let newOrder = [];
+      if (vm.orders.length) {
+        newOrder = vm.orders.sort((a, b) => {
+          const aIsPaid = a.is_paid ? 1 : 0;
+          const bIsPaid = b.is_paid ? 1 : 0;
+          return bIsPaid - aIsPaid;
+        });
+      }
+      return newOrder;
+    },
+  },
+  created() {
+    this.getOrders();
+    console.log(process.env.APIPATH);
+  },
+};
+</script>
